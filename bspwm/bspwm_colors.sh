92ec19b5bc5707b3bba64b95c2e49120f982aa09
Taritha <nolandonaldson@gmail.com>
2022-05-13 17:36:07 -0600

    Updated .gitignore again

--- bspwm/bspwm_colors.sh
+++ bspwm/bspwm_colors.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-bspc config focused_border_color "#e9b57d"
-bspc config normal_border_color  "#100f0e"
-bspc config active_border_color "#100f0e"
-bspc config presel_feedback_color   "#e9b57d"
+bspc config focused_border_color "#e46363"
+bspc config normal_border_color  "#0b0a0c"
+bspc config active_border_color "#0b0a0c"
+bspc config presel_feedback_color   "#e46363"
--- wpg/wp_init.sh
+++ wpg/wp_init.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-wpg -rs 'wallhaven-ymp7qd.jpg' 'wallhaven-ymp7qd.jpg'
+wpg -rs 'wallhaven-nmxrjy.jpg' 'wallhaven-nmxrjy.jpg'
