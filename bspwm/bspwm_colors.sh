5df9192cb516481f9e079897b5962765e601b71d
Taritha <nolandonaldson@gmail.com>
2022-06-24 01:56:13 -0600

    Added memory plots widget+window

--- bspwm/bspwm_colors.sh
+++ bspwm/bspwm_colors.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-bspc config focused_border_color "#d4ffff"
-bspc config normal_border_color  "#151b22"
-bspc config active_border_color "#151b22"
-bspc config presel_feedback_color   "#d4ffff"
+bspc config focused_border_color "#f56b56"
+bspc config normal_border_color  "#0e1012"
+bspc config active_border_color "#0e1012"
+bspc config presel_feedback_color   "#f56b56"
--- wpg/wp_init.sh
+++ wpg/wp_init.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-wpg -rs 'wallhaven-j3xd7w.jpg' 'wallhaven-j3xd7w.jpg'
+wpg -rs 'wallhaven-1jrkjv.png' 'wallhaven-1jrkjv.png'
