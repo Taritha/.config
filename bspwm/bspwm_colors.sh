6ba22db21e65c2abdab8a8f258bd5ded838ff0f8
Taritha <nolandonaldson@gmail.com>
2022-06-27 11:15:29 -0600

    Added filesystem widget to info center

--- bspwm/bspwm_colors.sh
+++ bspwm/bspwm_colors.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-bspc config focused_border_color "#d86b66"
-bspc config normal_border_color  "#0E0E0F"
-bspc config active_border_color "#0E0E0F"
-bspc config presel_feedback_color   "#d86b66"
+bspc config focused_border_color "#ff4857"
+bspc config normal_border_color  "#0A0C0E"
+bspc config active_border_color "#0A0C0E"
+bspc config presel_feedback_color   "#ff4857"
