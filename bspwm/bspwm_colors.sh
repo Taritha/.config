0191ec623b76c341f195a69bb755ea19cda35797
Taritha <nolandonaldson@gmail.com>
2022-09-02 14:52:54 -0600

    Resize UI icons

--- bspwm/bspwm_colors.sh
+++ bspwm/bspwm_colors.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-bspc config focused_border_color "#ffffc8"
-bspc config normal_border_color  "#252520"
-bspc config active_border_color "#252520"
-bspc config presel_feedback_color   "#ffffc8"
+bspc config focused_border_color "#db6c3f"
+bspc config normal_border_color  "#1c0f0a"
+bspc config active_border_color "#1c0f0a"
+bspc config presel_feedback_color   "#db6c3f"
