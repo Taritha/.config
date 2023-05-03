2ab34095c1e05e9f0f788f228c22bb345560ddd2
Taritha <nolandonaldson@gmail.com>
2023-05-03 17:53:22 -0500

    Add superslicer config to gitignore

--- bspwm/bspwm_colors.sh
+++ bspwm/bspwm_colors.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-bspc config focused_border_color "#6d484e"
-bspc config normal_border_color  "#0a0a0b"
-bspc config active_border_color "#0a0a0b"
-bspc config presel_feedback_color   "#6d484e"
+bspc config focused_border_color "#dca3ff"
+bspc config normal_border_color  "#120e3a"
+bspc config active_border_color "#120e3a"
+bspc config presel_feedback_color   "#dca3ff"
--- wpg/wp_init.sh
+++ wpg/wp_init.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-wpg -rs 'wallhaven-28g92m.jpg' 'wallhaven-28g92m.jpg'
+wpg -rs 'ciwhb2atipv81.jpg' 'ciwhb2atipv81.jpg'
