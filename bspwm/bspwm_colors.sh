0a8f457548d2e09078995527eea2187c342d89d2
Taritha <nolandonaldson@gmail.com>
2023-05-07 21:12:20 -0600

    Add superslicer and KiCAD to .gitignore

--- bspwm/bspwm_colors.sh
+++ bspwm/bspwm_colors.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-bspc config focused_border_color "#cc5936"
-bspc config normal_border_color  "#0C0A03"
-bspc config active_border_color "#0C0A03"
-bspc config presel_feedback_color   "#cc5936"
+bspc config focused_border_color "#ffdcf7"
+bspc config normal_border_color  "#0C0B0E"
+bspc config active_border_color "#0C0B0E"
+bspc config presel_feedback_color   "#ffdcf7"
--- wpg/wp_init.sh
+++ wpg/wp_init.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-wpg -rs 'wallhaven-96ej6d.jpg' 'wallhaven-96ej6d.jpg'
+wpg -rs '20CIEWE.jpg' '20CIEWE.jpg'
