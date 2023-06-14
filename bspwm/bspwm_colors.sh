dfc39fda492eb2c8e16f31a2e0c59efab6f3891e
Taritha <nolandonaldson@gmail.com>
2023-06-14 15:06:59 -0500

    Ignore superslicer and winapps

--- bspwm/bspwm_colors.sh
+++ bspwm/bspwm_colors.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-bspc config focused_border_color "#4a459e"
-bspc config normal_border_color  "#0C0A14"
-bspc config active_border_color "#0C0A14"
-bspc config presel_feedback_color   "#4a459e"
+bspc config focused_border_color "#DDB4B5"
+bspc config normal_border_color  "#0c1e2b"
+bspc config active_border_color "#0c1e2b"
+bspc config presel_feedback_color   "#DDB4B5"
--- wpg/wp_init.sh
+++ wpg/wp_init.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-wpg -rs 'ua9t3lo49hs81.png' 'ua9t3lo49hs81.png'
+wpg -rs 'HVtFzWG.png' 'HVtFzWG.png'
